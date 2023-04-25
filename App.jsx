f4bf0f2441d85f30d0b60ce871eba7f2454d4651
Kipngetich Ruto <ruto365@gmail.com>
2023-04-25 23:13:56 +0300

    The initial commit

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import Demo from "./components/Demo"
+import Hero from "./components/Hero"
+
+import './styles/App.css'
+
+const App = () => {
+  return (
+    <main>
+        <div className="main">
+            <div className="gradient"/>
+        </div>
+        <div className="app">
+            <Hero />
+            <Demo />
+        </div>
+    </main>
+  )
+}
+
+export default App
